33e463d8377916d4ed81fda63e173ecfcfcaa89a
FronkTP <96272954+FronkTP@users.noreply.github.com>
2025-09-03 23:34:07 +0700

    create colours configuration file

---
+++ react/src/COLOURS.js
@@ -0,0 +1,14 @@
+export default {
+    light: {
+        type: 'color',
+        value: "rgb(242, 241, 234)"
+    },
+    dark: {
+        type: 'color', 
+        value: "rgb(40, 54, 24)"
+    },
+    bubbleGum: {
+        type: 'gradient',
+        value: "linear-gradient(to top, #fbc2eb 0%, #a6c1ee 100%)"
+    }
+};
