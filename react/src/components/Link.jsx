389bfb8021e943d470b91438274ab5982118d708
FronkTP <96272954+FronkTP@users.noreply.github.com>
2025-09-04 23:58:50 +0700

    add link form ui and styles (no functionality yet)

--- react/src/components/Link.jsx
+++ react/src/components/Link.jsx
@@ -8,7 +8,31 @@ export default function Link({ toggleLink }) {
             <i className="fa-solid fa-xmark"></i>
           </button>
         </div>
-        <div className="modal__body">hello link</div>
+        <div className="modal__body">
+          <div className="link__form">
+            <div className="link__container-title">
+              <label for="link-input-title">Title</label>
+              <input
+                id="link-input-title"
+                className="link__input"
+                type="text"
+                placeholder="Greatest link"
+              />
+            </div>
+            <div className="link__container-url">
+              <label for="link-input-url">Url</label>
+              <input
+                id="link-input-url"
+                className="link__input"
+                type="text"
+                placeholder="example.com"
+              />
+            </div>
+            <button className="link__add-btn" type="submit">
+              Add
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
