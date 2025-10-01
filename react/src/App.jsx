a720ee5b079bfe60081b68903148f22327aa6ee3
FronkTP <96272954+FronkTP@users.noreply.github.com>
2025-10-01 23:27:08 +0700

    change default background

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -24,8 +24,9 @@ export default function App() {
   // Ui
   const [mode, setMode] = useState("dark");
   const [background, setBackground] = useState({
-    type: "color",
-    value: "dark", // color or image URL
+    type: "image",
+    value:
+      "https://images.unsplash.com/photo-1519451241324-20b4ea2c4220?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", // color or image URL
   });
   const [isMobile, setIsMobile] = useState(false);
   const [isTaskOpen, setIsTaskOpen] = useState(false);
