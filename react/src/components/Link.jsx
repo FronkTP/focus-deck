47c227cb6a4f7a3d55c39514d0f4d1f0c93ec79a
FronkTP <96272954+FronkTP@users.noreply.github.com>
2025-10-02 22:40:46 +0700

    create link addition functionality

--- react/src/components/Link.jsx
+++ react/src/components/Link.jsx
@@ -1,4 +1,24 @@
+import { useState } from "react";
+
 export default function Link({ toggleLink }) {
+  const [links, setLinks] = useState([]);
+  const [titleInput, setTitleInput] = useState("");
+  const [urlInput, setUrlInput] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLinks((prev) => [...prev, { title: titleInput, url: urlInput }]);
+    setTitleInput("");
+    setUrlInput("");
+    console.log(links);
+  };
+
+  const handleDelete = (index) => {
+    setLinks(links.filter((_, i) => i !== index));
+  };
+
+  const renderLinks = () => {};
+
   return (
     <div className="overlay" onClick={toggleLink}>
       <div className="modal" onClick={(e) => e.stopPropagation()}>
@@ -9,29 +29,61 @@ export default function Link({ toggleLink }) {
           </button>
         </div>
         <div className="modal__body">
-          <div className="link__form">
+          <form className="link__form" onSubmit={handleSubmit}>
             <div className="link__container-title">
-              <label for="link-input-title">Title</label>
+              <label htmlFor="link-input-title">Title</label>
               <input
                 id="link-input-title"
                 className="link__input"
                 type="text"
-                placeholder="Greatest link"
+                value={titleInput}
+                onChange={(e) => setTitleInput(e.target.value)}
+                placeholder="My fav link"
               />
             </div>
             <div className="link__container-url">
-              <label for="link-input-url">Url</label>
+              <label htmlFor="link-input-url">Url</label>
               <input
                 id="link-input-url"
                 className="link__input"
                 type="text"
+                value={urlInput}
+                onChange={(e) => setUrlInput(e.target.value)}
                 placeholder="example.com"
               />
             </div>
             <button className="link__add-btn" type="submit">
               Add
             </button>
-          </div>
+          </form>
+          <hr></hr>
+          <ul className="link__list">
+            {links.map((link, index) => (
+              <li key={index} className="link__item">
+                <p className="link__text">
+                  <a
+                    href={
+                      link.url.startsWith("http")
+                        ? link.url
+                        : `http://${link.url}`
+                    }
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {link.title}
+                  </a>
+                </p>
+                <div className="link__item-btns-container">
+                  <button
+                    className="link__btns link__item-btns link__delete-btn"
+                    onClick={() => handleDelete(index)}
+                  >
+                    <i className="fa-solid fa-x"></i>
+                  </button>
+                </div>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </div>
